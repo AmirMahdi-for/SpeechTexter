9d7d17512ed0dd2283a94bd65865744bbc85db5e
AmirMahdi-for <amirmahdifor@gmail.com>
2025-02-21 09:06:11 -0800

    feat: check type of voice file

--- app/Requests/VoiceFileRequest.php
+++ app/Requests/VoiceFileRequest.php
@@ -9,8 +9,8 @@ class VoiceFileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file_url' => ['nullable', 'url'],
-            'file' => ['nullable', 'file', 'max:10240'],
+            'file_url' => ['nullable', 'url', 'regex:/\.(mp3|wav|webm|ogg|aac|flac|m4a|amr)$/i', ],
+            'file'     => ['nullable', 'file', 'max:10240', 'mimes:mp3,wav,webm,ogg,aac,flac,m4a,amr'],
         ];
     }
 
